b5263e2a1ff045a77e9f3b5c8d89c0d4230f2509
KamKoding <qkamarie@gmail.com>
2026-05-28 15:19:37 -0700

    navBarWIP

--- iTunes.js
+++ iTunes.js
@@ -1,7 +1,15 @@
 //itunes
+const input = document.querySelector('#search')
+const searchBtn = document.querySelector('#searchBtn')
+
+searchBtn.addEventListener('click', () => {
+    iTunes()
+})
 
 async function iTunes() {
-    const getTunes = await fetch(`https://itunes.apple.com/search?term=michaeljackson&entity=song`)
+    const searchTerm = input.value;
+    // const getTunes = await fetch(`https://itunes.apple.com/search?term=michaeljackson&entity=song`)
+    const getTunes = await fetch(`https://itunes.apple.com/search?term=${searchTerm}&media=music&limit=10`)
     const getTunesJson = await getTunes.json()
     getTunesJson.results.map((song) => {
         console.log(song.trackName)
@@ -9,6 +17,5 @@ async function iTunes() {
     // return getTunesJson
 }
 
-
-console.log(iTunes())
+//console.log(iTunes())
 
