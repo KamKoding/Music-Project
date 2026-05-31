f917e1a12283b1262754cc680503143324b5a4e8
KamKoding <qkamarie@gmail.com>
2026-05-30 21:32:40 -0700

    home

--- iTunes.js
+++ iTunes.js
@@ -1,10 +1,12 @@
 //itunes
 const input = document.querySelector('#search')
 const searchBtn = document.querySelector('#searchBtn')
+const container = document.querySelector('#search__results')
 
 searchBtn.addEventListener('click', () => {
     iTunes()
 })
+
 
 async function iTunes() {
     const searchTerm = input.value;
@@ -15,7 +17,38 @@ async function iTunes() {
         console.log(song.trackName)
     })
     // return getTunesJson
+    const htmlPayload = getTunesJson.results.map((song) => 
+        `<div id="search__results--item">
+          <img
+            class="search__results--artwork"
+            src="${song.artworkUrl100}"
+            alt="Album Artwork"
+          />
+          <div class="search__results--info">
+            <h3 class="search__results--title">${song.trackName}</h3>
+            <p class="search__results--artist">${song.artistName}</p>
+          </div>
+        </div>`).join('')
+        console.log(container)
+    container.innerHTML = htmlPayload
 }
 
 //console.log(iTunes())
 
+// function renderTunes() {
+//     const htmlPayload = getTunesJson.results.map((song) => 
+//         `<div id="search__results--item">
+//           <img
+//             class="search__result--artwork"
+//             src="${song.artworkUrl100}"
+//             alt="Album Artwork"
+//           />
+//           <div class="search__results--info">
+//             <h3 class="search__results--title">${song.trackName}</h3>
+//             <p class="search__results--artist">${song.artistName}</p>
+//           </div>
+//         </div>`).join('')
+//     container.innerHTML = htmlPayload
+// }
+
+// renderTunes()
